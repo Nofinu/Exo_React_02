9ca1835a584a520ec83a3bfe3e1dbc79c5a2dcf8
Léo Lhomme <leolhommeo@gmail.com>
2023-02-09 11:20:07 +0100

    exo2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Clients } from './Component/Clients';
 import './index.css';
-import App from './App';
+import './component.css';
 import reportWebVitals from './reportWebVitals';
+
+const listeClients = [{nom:"nom1",prenom:"prenom1",telephone:"telephone1",statut:true,adresse:{rue:"laRue",ville:"laVille",codePostal:"01234"}},{nom:"nom2",prenom:"prenom2",telephone:"telephone2",statut:false,adresse:{rue:"laRue",ville:"laVille",codePostal:"01234"}}]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {listeClients.map((client,index)=><Clients client={client} key={index}></Clients>)}
   </React.StrictMode>
 );
 
